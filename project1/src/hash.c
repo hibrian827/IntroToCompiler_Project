685ab8ce757cbd06ed866e808257fdd17ec246c2
Brian Lee <hibrian827@gmail.com>
2025-09-21 00:57:41 +0900

    hash table added

--- project1/src/hash.c
+++ project1/src/hash.c
@@ -17,6 +17,50 @@ typedef struct nlist {
 
 static nlist *hashTable[HASH_TABLE_SIZE];
 
+unsigned hash(char *s) {
+    unsigned hashval = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        hashval = s[i] + 31 * hashval;
+    }
+    return hashval % HASH_TABLE_SIZE;
+}
+
 id *enter(int tokenType, char *name, int length) {
-  // TODO: Implement this function
+  unsigned index = hash(name);
+  nlist *np;
+
+  // Search the table
+  for (np = hashTable[index]; np != NULL; np = np->next) {
+    if (strncmp(name, np->data->name, length) == 0 && np->data->name[length] == '\0') {
+      np->data->count++;
+      return np->data;
+    }
+  }
+
+  // Not found -> create new entry
+  np = (nlist *)malloc(sizeof(*np));
+  if (np == NULL) return NULL;
+
+  id *newId = (id *)malloc(sizeof(*newId));
+  if (newId == NULL) {
+    free(np);
+    return NULL;
+  }
+
+  newId->tokenType = tokenType;
+  newId->count = 1;
+  newId->name = (char *)malloc(length + 1);
+  if (newId->name == NULL) {
+    free(newId);
+    free(np);
+    return NULL;
+  }
+  strncpy(newId->name, name, length);
+  newId->name[length] = '\0';
+
+  np->data = newId;
+  np->next = hashTable[index];
+  hashTable[index] = np;
+
+  return newId;
 }
